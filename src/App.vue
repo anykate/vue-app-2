bf251cc000fb11f5c8877bc227ccbe885c64ef25
Aniket Aryamane <aryamane.aniket@gmail.com>
2020-06-19 11:18:30 +0530

    Added new component AddProduct.vue with form

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
+    <AddProduct />
     <Products />
   </div>
 </template>
 
 <script>
+import AddProduct from "@/components/AddProduct.vue";
 import Products from "@/components/Products.vue";
 
 export default {
   name: "App",
   components: {
+    AddProduct,
     Products,
   },
 };
---
+++ src/components/AddProduct.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="add-product">
+    <h1>Add Product</h1>
+    <form @submit.prevent="onSubmit">
+      <input type="text" placeholder="Add Product..." v-model="newTitle" />
+      <input type="text" placeholder="Add Price..." v-model="newPrice" />
+      <input type="submit" value="submit" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddProduct",
+  data() {
+    return {
+      newTitle: "",
+      newPrice: "",
+    };
+  },
+  methods: {
+    onSubmit() {
+      console.log(this.newTitle + " " + this.newPrice);
+    },
+  },
+};
+</script>
+
+<style></style>
